1b1022b6a6b9346e72bd6d3299e8f1df92fe039c
Jacob Auger <54862261+jga9325@users.noreply.github.com>
2024-12-27 15:26:03 -0500

    Finish UserController and unit tests for it

--- src/main/java/com/auger/apartments/users/UserController.java
+++ src/main/java/com/auger/apartments/users/UserController.java
@@ -1,18 +1,17 @@
 package com.auger.apartments.users;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -22,6 +21,28 @@ public class UserController {
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User createdUser = userService.createUser(user);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable int id) {
+        Optional<User> user = userService.getUser(id);
+        if (user.isPresent()) {
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> allUsers = userService.getAllUsers();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        userService.updateUser(user);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- src/test/java/com/auger/apartments/users/UserControllerUnitTests.java
+++ src/test/java/com/auger/apartments/users/UserControllerUnitTests.java
@@ -1,6 +1,8 @@
 package com.auger.apartments.users;
 
+import com.auger.apartments.exceptions.DuplicateDataException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,12 +12,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -52,17 +54,181 @@ public class UserControllerUnitTests {
         verify(userService, times(1)).createUser(user);
     }
 
-    // Test correct responses for when a DuplicateDataException is thrown for email and phone number
+    @Test
+    public void testCreateUserDuplicateEmail() throws Exception {
+        User user = new User(1, "John", "john@gmail.com", "1234567894",
+                LocalDate.of(1999, 4, 28), LocalDate.now());
+
+        doThrow(new DuplicateDataException("A user with that email already exists"))
+                .when(userService).createUser(user);
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        MvcResult result = mockMvc.perform(post("/users")
+                .content(userJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        assertThat(responseString).isEqualTo("A user with that email already exists");
+
+        verify(userService, times(1)).createUser(user);
+    }
 
     @Test
-    public void testGetUser() {
+    public void testCreateUserDuplicatePhoneNumber() throws Exception {
+        User user = new User(1, "John", "john@gmail.com", "1234567894",
+                LocalDate.of(1999, 4, 28), LocalDate.now());
+
+        doThrow(new DuplicateDataException("A user with that phone number already exists"))
+                .when(userService).createUser(user);
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        MvcResult result = mockMvc.perform(post("/users")
+                .content(userJson)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        assertThat(responseString).isEqualTo("A user with that phone number already exists");
+
+        verify(userService, times(1)).createUser(user);
+    }
+
+    @Test
+    public void testGetUser() throws Exception {
         User user = new User(1, "John", "john@gmail.com", "1234567894",
                 LocalDate.of(1999, 4, 28), LocalDate.now());
 
         when(userService.getUser(user.id())).thenReturn(Optional.of(user));
 
-//        MvcResult result = mockMvc.perform(get("/users/{id}", user.id()
-//                .)
+        MvcResult result = mockMvc.perform(get("/users/{id}", user.id()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        User retrievedUser = objectMapper.readValue(responseString, User.class);
+
+        assertUsersAreEqual(user, retrievedUser);
+        verify(userService, times(1)).getUser(user.id());
+    }
+
+    @Test
+    public void testGetUserInvalidId() throws Exception {
+        int userId = 1;
+
+        when(userService.getUser(userId)).thenReturn(Optional.empty());
+
+        mockMvc.perform(get("/users/{id}", userId))
+                .andExpect(status().isNotFound());
+
+        verify(userService, times(1)).getUser(userId);
+    }
+
+    @Test
+    public void testGetAllUsers() throws Exception {
+        User user1 = new User(1, "John", "john@gmail.com", "1234567894",
+                LocalDate.of(1999, 4, 28), LocalDate.now());
+        User user2 = new User(2, "Bob", "bob@gmail.com", "6015234567",
+                LocalDate.of(1982, 7, 14), LocalDate.now());
+        User user3 = new User(3, "Beth", "beth@gmail.com", "9876420341",
+                LocalDate.of(1990, 2, 17), LocalDate.now());
+
+        List<User> userList = List.of(user1, user2, user3);
+        when(userService.getAllUsers()).thenReturn(userList);
+
+        MvcResult result = mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        CollectionType collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, User.class);
+        List<User> allUsers = objectMapper.readValue(responseString, collectionType);
+
+        assertThat(allUsers).isEqualTo(userList);
+        verify(userService, times(1)).getAllUsers();
+    }
+
+    @Test
+    public void testGetAllUsersEmptyResponse() throws Exception {
+        List<User> emptyList = List.of();
+
+        when(userService.getAllUsers()).thenReturn(emptyList);
+
+        MvcResult result = mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        CollectionType collectionType = objectMapper.getTypeFactory().constructCollectionType(List.class, User.class);
+        List<User> emptyListResponse = objectMapper.readValue(responseString, collectionType);
+
+        assertThat(emptyListResponse).isEqualTo(emptyList);
+        verify(userService, times(1)).getAllUsers();
+    }
+
+    @Test
+    public void testUpdateUser() throws Exception {
+        User user = new User(1, "John", "john@gmail.com", "1234567894",
+                LocalDate.of(1999, 4, 28), LocalDate.now());
+
+        doNothing().when(userService).updateUser(user);
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mockMvc.perform(put("/users")
+                        .content(userJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(userService, times(1)).updateUser(user);
+    }
+
+    @Test
+    public void testUpdateUserDuplicateEmail() throws Exception {
+        User user = new User(1, "John", "john@gmail.com", "1234567894",
+                LocalDate.of(1999, 4, 28), LocalDate.now());
+
+        doThrow(new DuplicateDataException("A user with that email already exists"))
+                .when(userService).updateUser(user);
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        MvcResult result = mockMvc.perform(put("/users")
+                        .content(userJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        assertThat(responseString).isEqualTo("A user with that email already exists");
+
+        verify(userService, times(1)).updateUser(user);
+    }
+
+    @Test
+    public void testUpdateUserDuplicatePhoneNumber() throws Exception {
+        User user = new User(1, "John", "john@gmail.com", "1234567894",
+                LocalDate.of(1999, 4, 28), LocalDate.now());
+
+        doThrow(new DuplicateDataException("A user with that phone number already exists"))
+                .when(userService).updateUser(user);
+
+        String userJson = objectMapper.writeValueAsString(user);
+
+        MvcResult result = mockMvc.perform(put("/users")
+                        .content(userJson)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isConflict())
+                .andReturn();
+
+        String responseString = result.getResponse().getContentAsString();
+        assertThat(responseString).isEqualTo("A user with that phone number already exists");
+
+        verify(userService, times(1)).updateUser(user);
     }
 
     private void assertUsersAreEqual(User u1, User u2) {
