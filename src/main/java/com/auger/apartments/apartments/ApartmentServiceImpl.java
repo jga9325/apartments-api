c6108d38914a38781d86e5f73ea4f47269de1681
Jacob Auger <54862261+jga9325@users.noreply.github.com>
2024-12-31 14:21:05 -0500

    Add final unit tests for ApartmentServiceImpl
    - Added unit tests for updateApartment() and doesExist()
    - Finished implementation and testing of the service layer for
      apartments

--- src/main/java/com/auger/apartments/apartments/ApartmentServiceImpl.java
+++ src/main/java/com/auger/apartments/apartments/ApartmentServiceImpl.java
@@ -1,7 +1,6 @@
 package com.auger.apartments.apartments;
 
 import com.auger.apartments.exceptions.ApartmentNotFoundException;
-import com.auger.apartments.exceptions.UserNotFoundException;
 
 import java.util.List;
 import java.util.Optional;
--- src/test/java/com/auger/apartments/apartments/ApartmentServiceImplUnitTests.java
+++ src/test/java/com/auger/apartments/apartments/ApartmentServiceImplUnitTests.java
@@ -1,5 +1,6 @@
 package com.auger.apartments.apartments;
 
+import com.auger.apartments.exceptions.ApartmentNotFoundException;
 import com.auger.apartments.exceptions.DuplicateDataException;
 import com.auger.apartments.exceptions.UserNotFoundException;
 import org.junit.jupiter.api.Test;
@@ -11,8 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -155,27 +155,136 @@ public class ApartmentServiceImplUnitTests {
 
     @Test
     public void testUpdateApartment() {
+        Apartment apartment = new Apartment(1, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, 1, null);
 
-    }
+        when(apartmentRepository.exists(apartment.id())).thenReturn(true);
+        doNothing().when(apartmentValidator).validateExistingApartment(apartment);
+        doNothing().when(apartmentRepository).update(apartment);
 
-    @Test
-    public void testUpdateApartmentInvalidOwner() {
+        assertThatNoException().isThrownBy(() -> underTest.updateApartment(apartment));
 
-    }
-
-    @Test
-    public void testUpdateApartmentInvalidRenter() {
-
-    }
-
-    @Test
-    public void testUpdateApartmentDuplicateRenter() {
-
+        verify(apartmentRepository, times(1)).exists(apartment.id());
+        verify(apartmentValidator, times(1)).validateExistingApartment(apartment);
+        verify(apartmentRepository, times(1)).update(apartment);
     }
 
     @Test
     public void testUpdateApartmentInvalidId() {
+        Apartment apartment = new Apartment(1, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, 1, null);
 
+        when(apartmentRepository.exists(apartment.id())).thenReturn(false);
+
+        assertThatThrownBy(() -> underTest.updateApartment(apartment))
+                .isInstanceOf(ApartmentNotFoundException.class)
+                .hasMessage(String.format("Apartment with id %s does not exist", apartment.id()));
+
+        verify(apartmentRepository, times(1)).exists(apartment.id());
+        verifyNoInteractions(apartmentValidator);
+        verify(apartmentRepository, times(0)).update(apartment);
+    }
+
+    @Test
+    public void testUpdateApartmentNullId() {
+        Apartment apartment = new Apartment(null, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, 1, null);
+
+        assertThatThrownBy(() -> underTest.updateApartment(apartment))
+                .isInstanceOf(ApartmentNotFoundException.class)
+                .hasMessage(String.format("Apartment with id %s does not exist", apartment.id()));
+
+        verifyNoInteractions(apartmentRepository);
+        verifyNoInteractions(apartmentValidator);
+    }
+
+    @Test
+    public void testUpdateApartmentInvalidOwner() {
+        Apartment apartment = new Apartment(1, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, 1, null);
+
+        when(apartmentRepository.exists(apartment.id())).thenReturn(true);
+        doThrow(new UserNotFoundException(String.format("User with id %s does not exist", apartment.ownerId())))
+                .when(apartmentValidator).validateExistingApartment(apartment);
+
+        assertThatThrownBy(() -> underTest.updateApartment(apartment))
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessage(String.format("User with id %s does not exist", apartment.ownerId()));
+
+        verify(apartmentRepository, times(1)).exists(apartment.id());
+        verify(apartmentValidator, times(1)).validateExistingApartment(apartment);
+        verify(apartmentRepository, times(0)).update(apartment);
+    }
+
+    @Test
+    public void testUpdateApartmentInvalidRenter() {
+        Apartment apartment = new Apartment(1, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, 1, 2);
+
+        when(apartmentRepository.exists(apartment.id())).thenReturn(true);
+        doThrow(new UserNotFoundException(String.format("User with id %s does not exist", apartment.renterId())))
+                .when(apartmentValidator).validateExistingApartment(apartment);
+
+        assertThatThrownBy(() -> underTest.updateApartment(apartment))
+                .isInstanceOf(UserNotFoundException.class)
+                .hasMessage(String.format("User with id %s does not exist", apartment.renterId()));
+
+        verify(apartmentRepository, times(1)).exists(apartment.id());
+        verify(apartmentValidator, times(1)).validateExistingApartment(apartment);
+        verify(apartmentRepository, times(0)).update(apartment);
+    }
+
+    @Test
+    public void testUpdateApartmentDuplicateRenter() {
+        Apartment apartment = new Apartment(1, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, 1, 2);
+
+        when(apartmentRepository.exists(apartment.id())).thenReturn(true);
+        doThrow(new DuplicateDataException(String.format("""
+                    A user with id %s is renting a different apartment.
+                    A user can only rent one apartment at a time.
+                    """, apartment.renterId())))
+                .when(apartmentValidator).validateExistingApartment(apartment);
+
+        assertThatThrownBy(() -> underTest.updateApartment(apartment))
+                .isInstanceOf(DuplicateDataException.class)
+                .hasMessage(String.format("""
+                    A user with id %s is renting a different apartment.
+                    A user can only rent one apartment at a time.
+                    """, apartment.renterId()));
+
+        verify(apartmentRepository, times(1)).exists(apartment.id());
+        verify(apartmentValidator, times(1)).validateExistingApartment(apartment);
+        verify(apartmentRepository, times(0)).update(apartment);
+    }
+
+    @Test
+    public void testDoesExist() {
+        int existingApartmentId = 1;
+        int nonExistingApartmentId = 2;
+
+        when(apartmentRepository.exists(existingApartmentId)).thenReturn(true);
+        when(apartmentRepository.exists(nonExistingApartmentId)).thenReturn(false);
+
+        assertThat(underTest.doesExist(existingApartmentId)).isTrue();
+        verify(apartmentRepository, times(1)).exists(existingApartmentId);
+
+        assertThat(underTest.doesExist(nonExistingApartmentId)).isFalse();
+        verify(apartmentRepository, times(1)).exists(nonExistingApartmentId);
+
+        assertThat(underTest.doesExist(null)).isFalse();
     }
 
     private void assertApartmentsAreEqual(Apartment a1, Apartment a2) {
