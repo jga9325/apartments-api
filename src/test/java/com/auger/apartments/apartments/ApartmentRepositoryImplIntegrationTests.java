9a31ce5046c4ae1cdc4d7daefa605ae2e662702b
Jacob Auger <54862261+jga9325@users.noreply.github.com>
2025-01-02 12:59:02 -0500

    Refactor repository integration tests
    - Updated beforeEach() in UserRepositoryImplIntegrationTests to create
      users that can be reused for each test
    - Created TestUtils class to store common assertion methods

--- src/test/java/com/auger/apartments/apartments/ApartmentRepositoryImplIntegrationTests.java
+++ src/test/java/com/auger/apartments/apartments/ApartmentRepositoryImplIntegrationTests.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+import static com.auger.apartments.TestUtils.assertApartmentsAreEqual;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Testcontainers
@@ -48,9 +49,9 @@ public class ApartmentRepositoryImplIntegrationTests {
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "apartments");
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
 
-        User newUser = new User(0, "John", "Rogers", "john@gmail.com",
+        User newUser = new User(null, "John", "Rogers", "john@gmail.com",
                 "1234567894", LocalDate.of(1999, 4, 28),
-                LocalDate.now());
+                null);
         user = userService.createUser(newUser);
 
         Apartment apt1 = new Apartment(null, "Main Street Condo",
@@ -111,7 +112,7 @@ public class ApartmentRepositoryImplIntegrationTests {
             assertApartmentsAreEqual(apartment, createdApartment);
             apartmentMap.remove(apartment.id());
         }
-        assertThat(apartmentMap.size()).isEqualTo(0);
+        assertThat(apartmentMap.size()).isZero();
     }
 
     @Test
@@ -149,21 +150,4 @@ public class ApartmentRepositoryImplIntegrationTests {
     private int getRowCount() {
         return JdbcTestUtils.countRowsInTable(jdbcTemplate, "apartments");
     }
-
-    private void assertApartmentsAreEqual(Apartment a1, Apartment a2) {
-        assertThat(a1.id()).isEqualTo(a2.id());
-        assertThat(a1.title()).isEqualTo(a2.title());
-        assertThat(a1.description()).isEqualTo(a2.description());
-        assertThat(a1.numberOfBedrooms()).isEqualTo(a2.numberOfBedrooms());
-        assertThat(a1.numberOfBathrooms()).isEqualTo(a2.numberOfBathrooms());
-        assertThat(a1.state()).isEqualTo(a2.state());
-        assertThat(a1.city()).isEqualTo(a2.city());
-        assertThat(a1.squareFeet()).isEqualTo(a2.squareFeet());
-        assertThat(a1.monthlyRent()).isEqualTo(a2.monthlyRent());
-        assertThat(a1.dateListed()).isEqualTo(a2.dateListed());
-        assertThat(a1.available()).isEqualTo(a2.available());
-        assertThat(a1.ownerId()).isEqualTo(a2.ownerId());
-        assertThat(a1.renterId()).isEqualTo(a2.renterId());
-    }
-
 }
--- src/test/java/com/auger/apartments/users/UserRepositoryImplIntegrationTests.java
+++ src/test/java/com/auger/apartments/users/UserRepositoryImplIntegrationTests.java
@@ -12,9 +12,13 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
-import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static com.auger.apartments.TestUtils.assertUsersAreEqual;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @Testcontainers
 @SpringBootTest
@@ -30,20 +34,34 @@ public class UserRepositoryImplIntegrationTests {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    private User user1;
+    private User user2;
+    private User user3;
+
     @BeforeEach
-    public void clearTable() {
+    public void setUp() {
         JdbcTestUtils.deleteFromTables(jdbcTemplate, "users");
+
+        User u1 = new User(null, "John", "Rogers", "john@gmail.com",
+                "1234567894", LocalDate.of(1999, 4, 28), null);
+        User u2 = new User(null, "Jennifer", "Lilly", "jennifer@gmail.com",
+                "9876543214", LocalDate.of(1975, 8, 3), null);
+        User u3 = new User(null, "Bob", "Daly", "Bob@gmail.com",
+                "7365490142", LocalDate.of(2001, 12, 19), null);
+        user1 = underTest.create(u1);
+        user2 = underTest.create(u2);
+        user3 = underTest.create(u3);
     }
 
     @Test
     public void testCreateAndFindOne() {
-        User user = new User(0, "John", "Rogers", "john@gmail.com",
-                "1234567894", LocalDate.of(1999, 4, 28), LocalDate.now());
+        User user = new User(null, "Conrad", "Wiggins", "conrad@gmail.com",
+                "4876280912", LocalDate.of(1972, 11, 14), null);
 
-        assertThat(getRowCount()).isEqualTo(0);
+        assertThat(getRowCount()).isEqualTo(3);
         User createdUser = underTest.create(user);
-        assertThat(getRowCount()).isEqualTo(1);
-        assertThat(createdUser.id()).isNotZero();
+        assertThat(getRowCount()).isEqualTo(4);
+        assertThat(createdUser.id()).isNotNull();
 
         Optional<User> optionalUser = underTest.findOne(createdUser.id());
         assertThat(optionalUser).isPresent();
@@ -53,48 +71,46 @@ public class UserRepositoryImplIntegrationTests {
 
     @Test
     public void testFindOneWithInvalidId() {
-        Optional<User> result = underTest.findOne(1);
+        Optional<User> result = underTest.findOne(0);
         assertThat(result).isNotPresent();
     }
 
     @Test
     public void testFindAll() {
-        User user1 = new User(0, "John", "Rogers", "john@gmail.com",
-                "1234567894", LocalDate.of(1999, 4, 28), LocalDate.now());
-        User user2 = new User(0, "Jennifer", "Lilly", "jennifer@gmail.com",
-                "9876543214", LocalDate.of(1975, 8, 3), LocalDate.now());
-        User user3 = new User(0, "Bob", "Daly", "Bob@gmail.com",
-                "7365490142", LocalDate.of(2001, 12, 19), LocalDate.now());
+        Map<Integer, User> userMap = new HashMap<>();
+        userMap.put(user1.id(), user1);
+        userMap.put(user2.id(), user2);
+        userMap.put(user3.id(), user3);
 
-        assertThat(getRowCount()).isEqualTo(0);
-        assertThat(underTest.findAll().size()).isEqualTo(0);
-        underTest.create(user1);
-        underTest.create(user2);
-        underTest.create(user3);
+        List<User> users = underTest.findAll();
+
         assertThat(getRowCount()).isEqualTo(3);
-        assertThat(underTest.findAll().size()).isEqualTo(3);
+        assertThat(users.size()).isEqualTo(3);
+
+        for (User user : users) {
+            User createdUser = userMap.get(user.id());
+            assertUsersAreEqual(user, createdUser);
+            userMap.remove(user.id());
+        }
+        assertThat(userMap.size()).isZero();
     }
 
     @Test
     public void testUpdate() {
-        User originalUser = new User(0, "John", "Rogers", "john@gmail.com",
-                "1234567894", LocalDate.of(1999, 4, 28), LocalDate.now());
-        User createdUser = underTest.create(originalUser);
-
-        int userId = createdUser.id();
-
-        User updatedUser = new User(userId, "Kai", "Asakura", "kai@gmail.com",
+        User updatedUser = new User(user1.id(), "Kai", "Asakura", "kai@gmail.com",
                 "7865436549", LocalDate.of(2003, 1, 18), null);
-        underTest.update(updatedUser);
-        assertThat(getRowCount()).isEqualTo(1);
 
-        Optional<User> optionalUser = underTest.findOne(userId);
+        underTest.update(updatedUser);
+
+        assertThat(getRowCount()).isEqualTo(3);
+
+        Optional<User> optionalUser = underTest.findOne(updatedUser.id());
         assertThat(optionalUser).isPresent();
 
-        User user = optionalUser.get();
-        User expectedUser = new User(userId, "Kai", "Asakura", "kai@gmail.com",
-                "7865436549", LocalDate.of(2003, 1, 18), LocalDate.now());
-        assertUsersAreEqual(user, expectedUser);
+        User retrievedUser = optionalUser.get();
+        User expectedUser = new User(updatedUser.id(), "Kai", "Asakura", "kai@gmail.com",
+                "7865436549", LocalDate.of(2003, 1, 18), user1.dateJoined());
+        assertUsersAreEqual(retrievedUser, expectedUser);
     }
 
     // Delete - Create a user, delete it, and check that it isn't there
@@ -103,23 +119,11 @@ public class UserRepositoryImplIntegrationTests {
 
     @Test
     public void testExists() {
-        assertThat(underTest.exists(1)).isFalse();
-        User user = new User(0, "John", "Rogers", "john@gmail.com",
-                "1234567894", LocalDate.of(1999, 4, 28), LocalDate.now());
-        User createdUser = underTest.create(user);
-        assertThat(underTest.exists(createdUser.id())).isTrue();
+        assertThat(underTest.exists(0)).isFalse();
+        assertThat(underTest.exists(user1.id())).isTrue();
     }
 
     private int getRowCount() {
         return JdbcTestUtils.countRowsInTable(jdbcTemplate, "users");
-    }
-
-    private void assertUsersAreEqual(User u1, User u2) {
-        assertThat(u1.firstName()).isEqualTo(u2.firstName());
-        assertThat(u1.lastName()).isEqualTo(u2.lastName());
-        assertThat(u1.email()).isEqualTo(u2.email());
-        assertThat(u1.phoneNumber()).isEqualTo(u2.phoneNumber());
-        assertThat(u1.birthDate()).isEqualTo(u2.birthDate());
-        assertThat(u1.dateJoined()).isEqualTo(u2.dateJoined());
     }
 }
