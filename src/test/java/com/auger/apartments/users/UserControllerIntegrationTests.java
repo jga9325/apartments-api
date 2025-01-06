6d1d873fadfb627100be267fea49ceb809557f91
Jacob Auger <54862261+jga9325@users.noreply.github.com>
2025-01-06 16:55:03 -0500

    Finish integration tests for UserController

--- src/test/java/com/auger/apartments/users/UserControllerIntegrationTests.java
+++ src/test/java/com/auger/apartments/users/UserControllerIntegrationTests.java
@@ -1,6 +1,8 @@
 package com.auger.apartments.users;
 
 import com.auger.apartments.BaseControllerIntegrationTest;
+import com.auger.apartments.apartments.Apartment;
+import com.auger.apartments.applications.Application;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -188,5 +190,146 @@ public class UserControllerIntegrationTests extends BaseControllerIntegrationTes
 
         assertThat(updateResponse.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
         assertThat(updateResponse.getBody()).isEqualTo("A user with that phone number already exists");
+    }
+
+    @Test
+    public void testDeleteUser() {
+        ResponseEntity<Void> deleteResponse = testRestTemplate
+                .exchange("/users/{id}", HttpMethod.DELETE, null, Void.class, user1.id());
+
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+
+        ResponseEntity<String> getResponse = testRestTemplate
+                .getForEntity("/users/{id}", String.class, user1.id());
+
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(getResponse.getBody())
+                .isEqualTo(String.format("User with id %s does not exist", user1.id()));
+    }
+
+    @Test
+    public void testDeleteUserInvalidId() {
+        int invalidUserId = 0;
+
+        ResponseEntity<String> deleteResponse = testRestTemplate.exchange("/users/{id}", HttpMethod.DELETE,
+                null, String.class, invalidUserId);
+
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(deleteResponse.getBody())
+                .isEqualTo(String.format("User with id %s does not exist", invalidUserId));
+    }
+
+    @Test
+    public void testDeleteUserIsRenter() {
+        Apartment apt1 = new Apartment(null, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, user1.id(), user2.id());
+        testRestTemplate.postForEntity("/apartments", apt1, Apartment.class);
+
+        ResponseEntity<String> deleteResponse = testRestTemplate.exchange("/users/{id}", HttpMethod.DELETE,
+                null, String.class, user2.id());
+
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+        assertThat(deleteResponse.getBody())
+                .isEqualTo(String.format("Unable to delete user with id %s because they are renting an apartment",
+                        user2.id()));
+    }
+
+    @Test
+    public void testDeleteUserOwnsOccupiedApartment() {
+        Apartment apt1 = new Apartment(null, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, user1.id(), user2.id());
+        testRestTemplate.postForEntity("/apartments", apt1, Apartment.class);
+
+        ResponseEntity<String> deleteResponse = testRestTemplate.exchange("/users/{id}", HttpMethod.DELETE,
+                null, String.class, user1.id());
+
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.CONFLICT);
+        assertThat(deleteResponse.getBody())
+                .isEqualTo(String.format(
+                        "Unable to delete user with id %s because they own at least one occupied apartment",
+                        user1.id()));
+    }
+
+    @Test
+    public void testDeleteUserWithApartmentsAndApplications() {
+        Apartment apt1 = new Apartment(null, "Main Street Condo",
+                "A spacious condo with brand new appliances and great views!", 2,
+                1, "NY", "New York", 800, 608900,
+                null, true, user1.id(), null);
+        Apartment apt2 = new Apartment(null, "Suburban Getaway",
+                "Entire unit in a quiet neighborhood", 3,
+                2, "VA", "Norfolk", 1400, 310000,
+                null, true, user1.id(), null);
+        Apartment apt3 = new Apartment(null, "Beach Apartment",
+                "One bed one bath apartment near the beach!", 1,
+                1, "FL", "Miami", 800, 185000,
+                null, true, user2.id(), null);
+        Apartment apartment1 = testRestTemplate
+                .postForEntity("/apartments", apt1, Apartment.class).getBody();
+        Apartment apartment2 = testRestTemplate
+                .postForEntity("/apartments", apt2, Apartment.class).getBody();
+        Apartment apartment3 = testRestTemplate
+                .postForEntity("/apartments", apt3, Apartment.class).getBody();
+
+        Application a1 = new Application(null, null, true, false,
+                user1.id(), apartment3.id());
+        Application a2 = new Application(null, null, true, false,
+                user2.id(), apartment1.id());
+        Application a3 = new Application(null, null, true, false,
+                user3.id(), apartment1.id());
+        Application a4 = new Application(null, null, true, false,
+                user2.id(), apartment2.id());
+        Application a5 = new Application(null, null, true, false,
+                user3.id(), apartment2.id());
+
+        Application app1 = testRestTemplate.postForEntity("/applications", a1, Application.class).getBody();
+        Application app2 = testRestTemplate.postForEntity("/applications", a2, Application.class).getBody();
+        Application app3 = testRestTemplate.postForEntity("/applications", a3, Application.class).getBody();
+        Application app4 = testRestTemplate.postForEntity("/applications", a4, Application.class).getBody();
+        Application app5 = testRestTemplate.postForEntity("/applications", a5, Application.class).getBody();
+
+        List<Integer> apartmentIdList = List.of(apartment1.id(), apartment2.id(), apartment3.id());
+        List<Integer> applicationListId = List.of(app1.id(), app2.id(), app3.id(), app4.id(), app5.id());
+
+        for (Integer id : apartmentIdList) {
+            ResponseEntity<Apartment> getResponse = testRestTemplate
+                    .getForEntity("/apartments/{id}", Apartment.class, id);
+            assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
+
+        for (Integer id : applicationListId) {
+            ResponseEntity<Application> getResponse = testRestTemplate
+                    .getForEntity("/applications/{id}", Application.class, id);
+            assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
+
+        testRestTemplate.exchange("/users/{id}", HttpMethod.DELETE, null, String.class, user1.id());
+
+        ResponseEntity<String> apartment1Response = testRestTemplate
+                .getForEntity("/apartments/{id}", String.class, apartment1.id());
+        assertThat(apartment1Response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(apartment1Response.getBody()).isEqualTo(String.format("Apartment with id %s does not exist",
+                apartment1.id()));
+
+        ResponseEntity<String> apartment2Response = testRestTemplate
+                .getForEntity("/apartments/{id}", String.class, apartment2.id());
+        assertThat(apartment2Response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(apartment2Response.getBody()).isEqualTo(String.format("Apartment with id %s does not exist",
+                apartment2.id()));
+
+        ResponseEntity<Apartment> apartment3Response = testRestTemplate
+                .getForEntity("/apartments/{id}", Apartment.class, apartment3.id());
+        assertThat(apartment3Response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        for (Integer id : applicationListId) {
+            ResponseEntity<String> getResponse = testRestTemplate
+                    .getForEntity("/applications/{id}", String.class, id);
+            assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+            assertThat(getResponse.getBody()).isEqualTo(String.format("Application with id %s does not exist", id));
+        }
     }
 }
